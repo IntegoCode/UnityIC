e8727eb45b563a065cacb086041ef0ef9c6e4fd1
Andrey <andrfomin98@gmail.com>
2021-02-28 17:19:31 +0200

    CameraFitSprite Implementation

---
+++ Assets/UnityIC/Camera/CameraFitSprite.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityIC
+{
+    [RequireComponent(typeof(Camera))]
+    public class CameraFitSprite : MonoBehaviour
+    {
+        [SerializeField]
+        private SpriteRenderer m_Sprite = null;
+
+        [SerializeField]
+        private FitType m_FitType = default;
+
+        private Camera m_Camera = null;
+
+        private void Awake()
+        {
+            m_Camera = GetComponent<Camera>();
+
+            FitSprite();
+        }
+
+        private void FitSprite()
+        {
+            float screenRatio = (float)Screen.width / (float)Screen.height;
+            float targetRatio = m_Sprite.bounds.size.x / m_Sprite.bounds.size.y;
+
+            float differenceInSize = targetRatio / screenRatio;
+
+            switch (m_FitType)
+            {
+                case FitType.Full:
+                    if (differenceInSize > 1)
+                    {
+                        Camera.main.orthographicSize = m_Sprite.bounds.size.y / 2;
+                    }
+                    else
+                    {
+                        Camera.main.orthographicSize = (m_Sprite.bounds.size.y / 2) * differenceInSize;
+                    }
+                    break;
+                case FitType.Horizontal:
+                    Camera.main.orthographicSize = (m_Sprite.bounds.size.y / 2) * differenceInSize;
+                    break;
+                case FitType.Vertical:
+                    Camera.main.orthographicSize = m_Sprite.bounds.size.y / 2;
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        private enum FitType
+        {
+            Full = 0,
+            Horizontal = 1,
+            Vertical = 2
+        }
+    }
+}
---
+++ Assets/UnityIC/Pool/Pool.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace UnityIC
+{
+    public class Pool<T> where T : MonoBehaviour 
+    {
+        private List<T> m_Objects = new List<T>();
+
+        private Predicate<T> m_ActiveObjectCondition = item => item.gameObject.activeInHierarchy;
+
+        public Pool() { }
+
+        public Pool(T originalObject, int number = 1, Action<T> action = null)
+        {
+            InstantiateObjects(originalObject, number, action);
+        }
+
+        public Pool<T> InstantiateObjects(T originalObject, int number = 1, Action<T> action = null)
+        {
+            Instantiate(originalObject, number, action);
+
+            return this;
+        }
+
+        public Pool<T> InstantiateObjects(IEnumerable<T> originalObjects, int number = 1, Action<T> action = null)
+        {
+            List<T> originalObjectList = originalObjects.ToList();
+
+            for (int j = 0; j < originalObjectList.Count; j++)
+            {
+                Instantiate(originalObjectList[j], number, action);
+            }
+
+            return this;
+        }
+
+        public Pool<T> SetActiveObjectCondition(Predicate<T> predicate)
+        {
+            m_ActiveObjectCondition = predicate;
+
+            return this;
+        }
+
+        public T GetObject(ObjecState state = ObjecState.NotActive)
+        {
+            T poolObject = null;
+
+            switch (state)
+            {
+                case ObjecState.Active:
+                    poolObject = m_Objects.Where(item => m_ActiveObjectCondition.Invoke(item)).FirstOrDefault();
+                    break;
+                case ObjecState.NotActive:
+                    poolObject = m_Objects.Where(item => !m_ActiveObjectCondition.Invoke(item)).FirstOrDefault();
+                    break;
+                case ObjecState.All:
+                    poolObject = m_Objects.FirstOrDefault();
+                    break;
+                default:
+                    break;
+            }
+
+            return poolObject;
+        }
+
+        public IEnumerable<T> GetObjects(ObjecState state = ObjecState.NotActive)
+        {
+            IEnumerable<T> objects = null;
+
+            switch (state)
+            {
+                case ObjecState.Active:
+                    objects = m_Objects.Where(item => m_ActiveObjectCondition.Invoke(item));
+                    break;
+                case ObjecState.NotActive:
+                    objects = m_Objects.Where(item => !m_ActiveObjectCondition.Invoke(item));
+                    break;
+                case ObjecState.All:
+                    objects = m_Objects;
+                    break;
+                default:
+                    break;
+            }
+
+            return objects;
+        }
+
+        private void Instantiate(T originalObject, int number = 1, Action<T> action = null)
+        {
+            for (int i = 0; i < number; i++)
+            {
+                T newObject = UnityEngine.Object.Instantiate(originalObject);
+                action?.Invoke(newObject);
+                m_Objects.Add(newObject);
+            }
+        }
+
+        public enum ObjecState
+        {
+            Active = 0,
+            NotActive = 1,
+            All = 2,
+        }
+    }
+}
